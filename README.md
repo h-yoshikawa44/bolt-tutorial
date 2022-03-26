78f07e7185e642ce55f7baa7b14a1d30314dc165
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2022-03-26 13:24:53 +0900

    転送・返信のBoltロジック検証

--- README.md
+++ README.md
@@ -3,24 +3,30 @@
 
 教材出典：[Bolt チュートリアル](https://slack.dev/bolt-js/ja-jp/tutorial/getting-started)
 
+プラス、色々検証したやつ。
+
 ## 環境
 - Node.js：16.13.0
 
-前提：[nglok](https://ngrok.com/) 導入済み
+前提：[ngrok](https://ngrok.com/) 導入済み
 
 ## Slack App の用意
 Slack ワークスペースと、アプリ（+ Bot）を作成してワークスペースにインストールしておく。
 
 ### Bot トークンのスコープについて
-- サイドバーの「OAuth & Permissions」へ
+- アプリ管理画面のサイドバーの「OAuth & Permissions」へ
 - Bot Token Scopes の欄に行き、「Add OAuth Scope」でスコープを設定（最低限、以下があれば OK）
-  - channels:history
-  - chat:write：アプリにボットユーザがメンバーとして参加しているチャンネルへのメッセージの投稿を許可する
+  - channels:history：アプリが連携されたチャンネルの履歴へのアクセスを許可する
+  - chat:write.public：アプリが連携されてないチャンネルへもメッセージの投稿を許可する
+  - chat:write：アプリが連携されたチャンネルへのメッセージの投稿を許可する
+  - reactions:read：アプリが連携されたチャンネルと会話で絵文字リアクションや関連するコンテンツへのアクセスを許可する
 
 ### 環境変数
 Bolt を使用するために以下の情報が必要になるので環境変数に設定。
 - ボットトークン： OAuth & Permissions の Bot User OAuth Token から
 - signing secret：Basic Information の Signing Secret から
+- Slack ワークスペースの URL：ワークスペース名のところから
+- 転送するチャンネル ID：任意のチャンネル ID
 
 ```bash
 # 作成した .env に環境変数を設定
@@ -28,26 +34,26 @@ cp .env.example .env
 ```
 
 ### イベントの設定について
-あらかじめ、nglok で3000ポートを公開しておく。
-（起動ごとに URL が変わるので注意）
+あらかじめ、ngrok で3000ポートを公開しておく。  
+（起動ごと？に URL が変わるので注意）
 
 - アプリ管理画面のサイドバーから「Event Subscriptions」へ
 - Enable Events を ON にする
 - Request URL を追加する（イベントに対応する HTTP POST リクエストをこの URL に送る）
-  - URL は`nglok の 公開 URL`+`/slack/events`形式
+  - URL は`ngrok の 公開 URL`+`/slack/events`形式
   - この URL の検証が済むとチェックマークがつく
-- Subscribe to bot events で監視するイベントを登録
+- Subscribe to bot events で監視するイベント（必要なやつだけ）を登録
 （OAuth Scope にも追加されて、アプリ再インストールしてねと案内されるので、再度「許可する」）
-    - message.channels：アプリが追加されているパブリックチャンネルのメッセージを監視
-    - message.groups：アプリが追加されているプライベートチャンネルのメッセージを監視
+    - message.channels：アプリが連携しているパブリックチャンネルのメッセージを監視
+    - message.groups：アプリが連携しているプライベートチャンネルのメッセージを監視
     - message.im：アプリとユーザーのダイレクトメッセージを監視
-    - message.mpim：アプリが追加されているグループDMを監視
+    - message.mpim：アプリが連携しているグループDMを監視
+    - reaction_added：アプリが連携しているチャンネルの。カスタム絵文字リアクションを監視
 
 ### アクションの設定について
 - アプリ管理画面のサイドバーから「Interactivity & Shortcuts」を選択
 - Interactivity を ON にする
 - Request URL を入力（Bolt はデフォルトでは、イベントに使用しているのと同じエンドポイントをインタラクティブコンポーネントに使用するように設定されている）して保存
-  - （イベントの設定のところで設定したやつと同じで OK）
 
 ### Bot をチャンネルに追加
 検証したい Slack チャンネルに、作成した Bot のアプリを追加しておく。
--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 require('dotenv').config();
 const { App } = require('@slack/bolt');
 
+const workSpace = process.env.SLACK_WORK_SPACE_URL;
+const archives = 'archives';
+
 // ボットトークンとソケットモードハンドラーを使ってアプリを初期化します
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
@@ -28,15 +31,159 @@ app.message('hello', async ({ message, say }) => {
         }
       }
     ],
-    text: `Hey there <@${message.user}>!`
+    text: `Hey2 there <@${message.user}>!`
   });
 });
 
-app.action('button_click', async ({ body, ack, say }) => {
+app.event('reaction_added', async ({ event, say, client }) => {
+  await say(event.reaction);
+
+  if (event.reaction === 'eyes') {
+    const formatTs = event.item.ts.replace('.', '')
+
+    // TODO 転送先チャンネル情報取得
+    // TODO 転送先チャンネルの配列でまわして一つずつ投稿（Promise.allでも）
+    await client.chat.postMessage({
+      channel: process.env.SLACK_SHARE_CHANNEL,
+      blocks: [
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: `共有だよ　${workSpace}/${archives}/${event.item.channel}/p${formatTs}`
+          },
+        },
+        {
+          type: 'divider'
+        },
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: '返信する場合は、このボタンから転送元のスレッドに返信できるよ'
+          },
+        },
+        {
+          type: 'actions',
+          elements: [
+            {
+              type: 'button',
+              text: {
+                type: 'plain_text',
+                text: 'OKと返信',
+              },
+              action_id: 'rep_share_ok_button'
+            },
+            {
+              type: 'button',
+              text: {
+                type: 'plain_text',
+                text: 'NGと返信',
+              },
+              action_id: 'rep_share_ng_button'
+            },
+            {
+              type: 'button',
+              text: {
+                type: 'plain_text',
+                text: 'カスタムテキストで返信',
+              },
+              action_id: 'rep_share_custom_button'
+            }
+          ]
+        }
+      ],
+      text: `${workSpace}/${archives}/${event.item.channel}/p${formatTs}`
+    })
+  }
+});
+
+app.action('rep_share_ok_button', async ({ body, ack, client }) => {
   // アクションのリクエストを確認
   await ack();
-  await say(`<@${body.user.id}> clicked the button`);
+  console.log(body)
+
+  // 転送元 URL 部分を分解
+  const regex = /https:\/\/[\w-]+\.slack\.com\/archives\/([\w]+)\/p([\d.]+)/
+  const url = body.message.text.match(regex);
+  const channel = url[1];
+  const ts = url[2];
+
+  await client.chat.postMessage({
+    channel,
+    thread_ts: `${ts.substring(0, ts.length - 6)}.${ts.substring(ts.length - 6)}}`,
+    text: `<#${body.container.channel_id}> から返信：OK`
+  })
 });
+
+app.action('rep_share_ng_button', async ({ body, ack, client }) => {
+  await ack();
+
+  // 転送元 URL 部分を分解
+  const regex = /https:\/\/[\w-]+\.slack\.com\/archives\/([\w]+)\/p([\d.]+)/
+  const url = body.message.text.match(regex);
+  const channel = url[1];
+  const ts = url[2];
+
+  await client.chat.postMessage({
+    channel,
+    thread_ts: `${ts.substring(0, ts.length - 6)}.${ts.substring(ts.length - 6)}}`,
+    text: `<#${body.container.channel_id}> から返信：NG`
+  })
+});
+
+app.action('rep_share_custom_button', async ({ body, ack, client }) => {
+  await ack();
+
+  await client.views.open({
+    trigger_id: body.trigger_id,
+    view: {
+      type: 'modal',
+      // callback_id が view を特定するための識別子
+      callback_id: 'rep_share_custom_text',
+      title: {
+        type: 'plain_text',
+        text: 'カスタムテキストで返信'
+      },
+      blocks: [
+        {
+          type: 'input',
+          block_id: 'custom_response_block',
+          label: {
+            type: 'plain_text',
+            text: '返信する内容を書いてね'
+          },
+          element: {
+            type: 'plain_text_input',
+            action_id: 'custom_response',
+            multiline: true,
+          }
+        }
+      ],
+      submit: {
+        type: 'plain_text',
+        text: 'Submit'
+      },
+      private_metadata: body.message.text
+    },
+  })
+});
+
+app.view('rep_share_custom_text', async ({ ack, view, client }) => {
+  await ack();
+
+  const regex = /https:\/\/[\w-]+\.slack\.com\/archives\/([\w]+)\/p([\d.]+)/
+  const url = view.private_metadata.match(regex);
+  const channel = url[1];
+  const ts = url[2];
+
+  await client.chat.postMessage({
+    channel,
+    thread_ts: `${ts.substring(0, ts.length - 6)}.${ts.substring(ts.length - 6)}}`,
+    text: `<#${channel}> から返信：\n\n${view.state.values.custom_response_block.custom_response.value}`
+  });
+});
+
 
 (async () => {
   // アプリを起動します
